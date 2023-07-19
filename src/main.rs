550fa5d08581f03cd576ed68ba000395949cf82e
Cyril Plisko <cyril.plisko@mountall.com>
2023-07-19 19:52:47 +0300

    env and secrets

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+use std::collections::HashMap;
+use std::env;
+
+use axum::Json;
 use axum::{routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn environment() -> Json<HashMap<String, String>> {
+    let env = env::vars().collect();
+    Json(env)
+}
+
+async fn secrets(secrets: shuttle_secrets::SecretStore) -> Json<HashMap<String, String>> {
+    let secrets = secrets.into_iter().collect();
+    Json(secrets)
+}
+
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+async fn axum(
+    #[shuttle_secrets::Secrets] shuttle_secrets: shuttle_secrets::SecretStore,
+) -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/env", get(environment))
+        .route("/secrets", get(|| async { secrets(shuttle_secrets).await }));
 
     Ok(router.into())
 }
